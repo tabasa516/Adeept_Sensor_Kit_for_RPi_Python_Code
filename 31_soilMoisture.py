608160f9cc5b73ad0d6b40e07ccc4b15f6159486
tabasa516 <150096977+tabasa516@users.noreply.github.com>
2024-01-09 22:50:45 +0900

    Update 31_soilMoisture.py

--- 31_soilMoisture.py
+++ 31_soilMoisture.py
@@ -9,7 +9,7 @@ def loop():
 	while True:
 		res = ADC0832.getResult()
 		moisture = 255 - res
-		print 'analog value: %03d  moisture: %d' %(res, moisture)
+		print('analog value: %03d  moisture: %d' % (res, moisture))
 		time.sleep(0.1)
 
 if __name__ == '__main__':
